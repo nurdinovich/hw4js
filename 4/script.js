7ee6aa978809e7dc46f2d085e1c7808ec95d3c91
nurdinovich <149245590+nurdinovich@users.noreply.github.com>
2023-11-21 15:28:23 +0000

    Add files via upload

---
+++ 4/script.js
@@ -0,0 +1,55 @@
+const residKr = {
+amerika:{
+Population:334998398,
+Square:9834000 ,
+language:'English'
+},
+afrika:{
+    Population:334998398,
+    Square:9834000 ,
+    language:'English'
+},
+italia:{
+        Population:630000,
+        Square:9834 ,
+        language:'itali'
+},
+evropa:{
+        Population:334998398,
+        Square:9834000 ,
+        language:'evropa'
+},
+germani:{
+        Population:38398,
+        Square:4000 ,
+        language:'немисче'
+},
+russia:{
+    Population:398,
+    Square:98 ,
+    language:'орусча'
+    },
+tailant:{
+        Population:3398,
+        Square:94000 ,
+        language:'билбейм'
+    },
+rim:{
+            Population:48398,
+            Square: 300 ,
+            language:'римче'
+    },
+moskva:{
+            Population:334998,
+            Square:9834 ,
+            language:'орусча'
+    },
+antalia:{
+            Population:3398,
+            Square:98333,
+            language:'точно билбейм '
+    }
+
+
+}
+console.log(residKr.germani.Square);
